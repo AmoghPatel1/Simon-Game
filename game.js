3ac9ee06b7c09f8a8493d91a6948401fc58f2ccf
Amogh Patel <amogh.patel.2000@gmail.com>
2022-05-04 23:11:55 +0530

    Simon Game

---
+++ game.js
@@ -0,0 +1,73 @@
+let buttonColours = ["red", "blue", "green", "yellow"];
+
+let gamePattern = [];
+let userClickedPattern = [];
+let level = 0;
+let started = false;
+
+
+$(document).keypress(function (e) {
+    if(!started){
+        $("#level-title").text("Level " + level)
+        nextSequence();
+        started = true;
+    }
+});
+
+$(".btn").click(function () {
+    let userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+
+    checkAnswer(userClickedPattern.length-1);
+});
+
+function nextSequence() {
+    userClickedPattern = []  // This is very important. Array Resetting.
+    level += 1;   
+    $("#level-title").text("Level "+ level);
+    let randomNumber = Math.floor(Math.random() * 4);
+    let randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+}
+
+function playSound(name) {
+    let audio = new Audio("./sounds/"+name+".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function () {
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
+}
+
+function checkAnswer(currentLevel) {
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+        if(gamePattern.length === userClickedPattern.length){
+            setTimeout(function () {
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(() => {
+            $("body").removeClass("game-over");
+        }, 200);
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        
+        startOver();
+    }
+}
+
+function startOver() {
+    started = false;
+    level = 0;
+    gamePattern = []
+}
